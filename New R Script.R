97dbd1403df3311fa712037e96f43800189cb4f6
Eric <erice17@gmail.com>
2018-10-18 14:21:07 -0600

    New change to the file. Added additional text.

---
+++ New R Script.R
@@ -0,0 +1,11 @@
+#test script for GitHub
+myfunction <- function(x,y){
+  if(x>y){
+    print("x is greater than y")
+  }else{
+    print("y is greater than or equal to x")
+  }
+}
+#Changing this line of the code
+#I have no idea what I am doing!!!
+z<-myfunction(x=10,y=5)
